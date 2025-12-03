3d362403e29dd84f40619d90d39ffcff7c17c240
muhammetsahin2003 <muhammetsahin2003@gmail.com>
2025-12-02 21:52:23 -0500

    CounsellerPortalApplication

--- src/main/java/com/rukiyesahin/miniproject/controllers/EnquiryController.java
+++ src/main/java/com/rukiyesahin/miniproject/controllers/EnquiryController.java
@@ -1,4 +1,26 @@
 package com.rukiyesahin.miniproject.controllers;
 
+import com.rukiyesahin.miniproject.entities.Enquiry;
+import com.rukiyesahin.miniproject.service.EnquiryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EnquiryController {
+
+    private EnquiryService enqService;
+
+    public EnquiryController(EnquiryService enqService){
+        this.enqService = enqService;
+    }
+
+    @GetMapping("/enquiry")
+    public String addEnquiryPage(Model model){
+        Enquiry enqObj = new Enquiry();
+        model.addAttribute("enq", enqObj);
+        return "enquiryForm";
+    }
+
+
 }
