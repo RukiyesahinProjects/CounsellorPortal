ab75ad31a1135884e7d6c84a81bd1610b1cc07ba
muhammetsahin2003 <muhammetsahin2003@gmail.com>
2025-12-09 15:00:14 -0500

    CounsellerPortalApplication

--- src/main/java/com/rukiyesahin/miniproject/controllers/EnquiryController.java
+++ src/main/java/com/rukiyesahin/miniproject/controllers/EnquiryController.java
@@ -1,16 +1,20 @@
 package com.rukiyesahin.miniproject.controllers;
 
 import com.rukiyesahin.miniproject.dto.DashboardResponse;
+import com.rukiyesahin.miniproject.dto.ViewEnqsFilterRequest;
 import com.rukiyesahin.miniproject.entities.Enquiry;
 import com.rukiyesahin.miniproject.service.CounsellorService;
 import com.rukiyesahin.miniproject.service.EnquiryService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class EnquiryController {
@@ -18,6 +22,33 @@ public class EnquiryController {
     private EnquiryService enqService;
     public EnquiryController(EnquiryService enqService){
         this.enqService = enqService;
+    }
+
+    @PostMapping("/filter-enqs")
+    public String filterEnquries(ViewEnqsFilterRequest viewEnqsFilterRequest, HttpServletRequest req, Model model){
+        HttpSession session = req.getSession(false);
+        Integer counsellorId = (Integer) session.getAttribute("counsellorId");
+
+        List<Enquiry> enqList = enqService.getEnquiriesWithFilter(viewEnqsFilterRequest, counsellorId);
+        model.addAttribute("enquiries", enqList);
+
+        return "viewEnqsPage";
+    }
+    @GetMapping("/view-enquiries")
+    public String getEnquiries(HttpServletRequest request, Model model){
+
+        HttpSession session = request.getSession(false);
+        Integer counsellorId = (Integer) session.getAttribute("counsellorId");
+
+        List<Enquiry> enqList = enqService.getAllEnquiries(counsellorId);
+        model.addAttribute("enquiries", enqList);
+
+        //Search form binding object
+        ViewEnqsFilterRequest filterReq = new ViewEnqsFilterRequest();
+        model.addAttribute("viewEnqFilter", filterReq);
+
+
+        return "viewEnqsPage";
     }
 
     @GetMapping("/enquiry")
